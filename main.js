289002deaaa61400abe6aa5563d995e367713f1d
katiepalumbo <palumbo.katie@gmail.com>
2022-02-04 17:59:07 +0100

    palindromo prompt riuscito

---
+++ main.js
@@ -0,0 +1,13 @@
+//definisci variabili
+
+
+document.getElementById('palindromo').addEventListener('click', playPalindromo);
+
+function playPalindromo() {
+    console.log('Lets go!');
+    const parola = prompt('Inserisci una parola');
+    parola;
+    
+    console.log(parola);
+
+};
